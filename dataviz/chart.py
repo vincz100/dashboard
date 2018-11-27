85db4e7fabb4bc14e67cc120ecc3c0472c1e8666
Vincent Ferrand <vferrand@citadia.com>
2018-11-27 18:17:10 +0100

    test class LineChart

--- dataviz/chart.py
+++ dataviz/chart.py
@@ -1,24 +1,25 @@
-import pygal
-import pandas as pd
-from .models import Statistiques
+# import pygal
+# import pandas as pd
+# from .models import Statistiques
 
-class PopLineChart():
+# class LineChart():
 
-    def __init__(self, **kwargs):
-        self.chart = pygal.Line(**kwargs)
-        self.chart.title = 'Evolution de la population entre 1968 et 2015'
+#     def __init__(self, **kwargs):
+#         self.chart = pygal.Line(**kwargs)
+#         self.chart.title = 'Evolution de la population entre 1968 et 2015'
 
-    def get_data(self):
-        data = list(Statistiques.objects.values_list("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop").filter(codgeo='39038'))
-        return data
+#     def get_data(self):
+#         filtre = Statistiques.objects.values_list("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop").filter(codgeo='39230')
+#         data = [el for el in filtre[0]]
+#         return data
 
-    def generate(self):
-        # year = ["1968", "1975", "1982", "1990", "1999", "2010", "2015"]
-        chart_data = self.get_data()
+#     def generate(self):
+#         # year = ["1968", "1975", "1982", "1990", "1999", "2010", "2015"]
+#         chart_data = self.get_data()
         
-        # Add data to chart
-        # self.chart.x_labels = map(str, year)
-        self.chart.add("population", [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+#         # Add data to chart
+#         for el in chart_data:
+#             self.chart.add(el)
 
-        # Return the rendered SVG
-        return self.chart.render(is_unicode=True)
+#         # Return the rendered SVG
+#         return self.chart.render(is_unicode=True)
--- dataviz/views.py
+++ dataviz/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.template.response import TemplateResponse
 from django.http import HttpResponse, JsonResponse
+
 from datetime import datetime
 from dataviz.models import Statistiques
-from dataviz.chart import PopLineChart
+# from dataviz.chart import LineChart
+
 import pygal
 
 
@@ -12,15 +14,33 @@ import pygal
 # la requête HTTP initiale
 # le template home.html 
 # un dictionnaire reprenant les variables qui seront accessibles dans le template
+
+# class IndexView(TemplateView):
+# 	template_name = 'home.html'
+
+# 	def get_context_data(self, **kwargs):
+# 		context = super(IndexView, self).get_context_data(**kwargs)
+
+# 		chart1 = LineChart(
+# 			height=600,
+#             width=800,
+#             explicit_size=True
+# 		)
+
+# 		context['chart1'] = chart1.generate()
+# 		return context
+
 def home(request):
-	filtre = Statistiques.objects.values_list("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop").filter(codgeo='39313')
+	filtre = Statistiques.objects.values_list("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop").filter(codgeo='39230')
+	data = [el for el in filtre[0]]
 	data = []
 	for el in filtre[0]:
 		data.append(el)
-	line_chart = pygal.StackedLine(width=520)
-	line_chart = pygal.Line()
+
+	line_chart = pygal.Line(width=500, height=400, explicit_size=True)
 	line_chart.title = 'Evolution de la population depuis 1968'
 	line_chart.add("population", data)
+
 	return HttpResponse(line_chart.render())
 	
 	# data = Statistiques.objects.all()
@@ -28,6 +48,7 @@ def home(request):
 	# return render(request, "templates/home.html", {"date": datetime.now()})
 
 # page index avec la méthode HttpResponse(text) qui prend comme paramètre une chaîne de caractères et renvoie le HTML au navigateur. 
+
 def index(request):
     return HttpResponse("""
         <h1>SOCIO DEMO</h1>
