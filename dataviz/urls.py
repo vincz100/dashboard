7613f1ac5f00cb1fb8b88c8a1e1f7016138efd82
Vincent Ferrand <vferrand@citadia.com>
2018-12-03 18:06:39 +0100

    code cleaning

--- dataviz/urls.py
+++ dataviz/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from . import views
-from dataviz.views import HomeView, get_data, ChartData
+from dataviz.views import SocioDemo, get_data, ChartData
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='index'),
+    path('', views.accueil, name='index'),
+
     path('api/data/', get_data, name='api-data'),
-    path('api/chart/data/', ChartData.as_view()),
-    path('accueil/', views.accueil, name='accueil'),
-    path('socio_demo/', views.index2, name='index2'),
+
+    path('socio_demo/', SocioDemo.as_view(), name='socio-demo'),
+    path('socio_demo/api/chart/data/', ChartData.as_view()),
+
     path('eco_emploi/', views.index3, name='index3'),
 ]
 
--- dataviz/views.py
+++ dataviz/views.py
@@ -1,3 +1,6 @@
+#Standard library imports
+
+# Django imports
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
@@ -7,61 +10,30 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+# third-party
 from datetime import datetime
 import pygal
 
+# local Django
 from dataviz.models import Statistiques
-# from dataviz.chart import LineChart
 
-# page d'accueil "acceuil" avec la fonction render qui prend 3 arguments en paramètre : 
+# page "accueil" avec la fonction render qui prend 3 arguments en paramètre : 
 # la requête HTTP initiale
 # le template home.html 
 # un dictionnaire reprenant les variables qui seront accessibles dans le template
 
-# class IndexView(TemplateView):
-# 	template_name = 'home.html'
-
-# 	def get_context_data(self, **kwargs):
-# 		context = super(IndexView, self).get_context_data(**kwargs)
-
-# 		chart1 = LineChart(
-# 			height=600,
-#             width=800,
-#             explicit_size=True
-# 		)
-
-# 		context['chart1'] = chart1.generate()
-# 		return context
-
-def accueil(request):
-	filtre = Statistiques.objects.values_list("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop").filter(codgeo='39518')
-	data = [el for el in filtre[0]]
-
-	line_chart = pygal.Line(width=500, height=400, explicit_size=True)
-	line_chart.title = 'Evolution de la population depuis 1968'
-	line_chart.add("population", data)
-
-	return HttpResponse(line_chart.render())
-	
-	# data = Statistiques.objects.all()
-	# return TemplateResponse(request, "templates/home.html", { "data": data })
-	# return render(request, "templates/home.html", {"date": datetime.now()})
-
-# page index avec la méthode HttpResponse(text) qui prend comme paramètre une chaîne de caractères et renvoie le HTML au navigateur. 
-
-User = get_user_model()
-class HomeView(View):
+#
+class SocioDemo(View):
 	def get(self, request, *args, **kwargs):
 		return render(request, 'charts.html', {})
 
 def get_data(request, *args, **kwargs):
-	data = (Statistiques.objects.values("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop").filter(codgeo='39538'))[0]
+	data = (Statistiques.objects.values("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop")[0])
 	return JsonResponse(data)
 
 class ChartData(APIView):
 	authentication_classes = []
 	permission_classes = []
-
 	def get(self, request, format=None):
 		labels = ["1968", "1975", "1982", "1990", "1999", "2010", "2015"]
 		filtre = Statistiques.objects.values_list("d68_pop", "d75_pop", "d82_pop", "d90_pop", "d99_pop", "p10_pop", "p15_pop").filter(codgeo='39518')
@@ -71,6 +43,11 @@ class ChartData(APIView):
 			"default": default_items,
 		}
 		return Response(data)
+
+def accueil(request):
+	return HttpResponse("""
+        <h1>ACCUEIL</h1>
+        """)
 
 def index2(request):
     return HttpResponse("""
