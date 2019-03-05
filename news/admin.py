d026887e32397580f12c6e7ef5895eac9c144cc6
Vincent Ferrand <vincent.ferrand@deepki.com>
2019-03-05 18:20:33 +0100

    models config

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+admin.site.register(Post)
--- news/models.py
+++ news/models.py
@@ -1,8 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class News(models.Model):
-    news_id = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    mod_date = models.DateTimeField('date modified')
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
 
-class 
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __unicode__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    text = models.TextField()
+    creation_date = models.DateTimeField('date published')
+    modification_date = models.DateTimeField('date modified')
+    author = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)
+    categories = models.ManyToManyField((Category), blank=True, null=True, through='CategoryToPost')
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/%s/%s/%s/" % (self.creation_date.year, self.creation_date.month, self.slug)
+
+class Comment(models.Model):
+    pass
+
+class CategoryToPost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
