29b006e5fe05cd6324d7fcf8a9393f449c0d757b
Vincent Ferrand <vincent.ferrand@deepki.com>
2019-03-25 00:30:27 +0100

    class problem

--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,13 @@
-from . import views
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+
 from . import views
-from news.views import index, ArticlesPage
+from news.views import index, ArticlesPage, test
 
 urlpatterns = [
     path('front/', index),
-    path('front/articles/', ArticlesPage.as_view()),
+    path('front/test/', test),
+    path('front/articles/', ArticlesPage.as_view())
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,15 @@
-from django.urls import path, include
-from django.shortcuts import render
+"""Standard library imports"""
 
-from django.views.generic import TemplateView
+# Django imports
+from django.contrib.auth import get_user_model, authenticate, login
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, View
+from django.template.response import TemplateResponse
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
 from news.models import Article, Category, CategoryToPost
 
@@ -11,13 +19,17 @@ def index(request):
 # def articles(request):
 #     return render(request, 'home_news.html', {Article.objects.all().order_by('publication_date')[0:5]})
 
+
 class ArticlesPage(TemplateView):
     template_name = "home_news.html"
 
     def article_list(self, request):
-        # queryset = Article.objects.get(id=1)
-        queryset = Article.objects.order_by('publication_date')[0:5]
-        context = {
-            "articles": queryset
-        }
+        queryset = Article.objects.order_by('publication_date')[:5]
+        context = {"articles": queryset}
         return render(request, self.template_name, context)
+
+
+def test(request):
+    queryset = Article.objects.order_by('publication_date')[:5]
+    context = {"articles": queryset}
+    return render(request, "home_news.html", context)
