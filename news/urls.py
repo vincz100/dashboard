0991d55711dc26ebae38be054187ec4430bed631
Vincent Ferrand <vincent.ferrand@deepki.com>
2019-03-24 15:33:46 +0100

    organize folder

--- news/urls.py
+++ news/urls.py
@@ -4,8 +4,9 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 from . import views
-from news.views import ArticlesPage
+from news.views import index, ArticlesPage
 
 urlpatterns = [
+    path('front/', index),
     path('front/articles/', ArticlesPage.as_view()),
 ]
--- news/views.py
+++ news/views.py
@@ -5,12 +5,15 @@ from django.views.generic import TemplateView
 
 from news.models import Article, Category, CategoryToPost
 
+def index(request):
+    return render(request, 'index.html', {})
+
 class ArticlesPage(TemplateView):
     template_name = "home_news.html"
 
     def article_list(self, parameter_list):
         queryset = Article.objects.all().order_by('publication_date')[0:5]
         context = {
-            'articles': queryset
+            "articles": queryset
         }
         return render(request, self.template_name, context)
